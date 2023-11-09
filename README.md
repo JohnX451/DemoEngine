54b5f1cbc57caea59d85d79ad171f4f0909374e3
JohnX451 <50927491+JohnX451@users.noreply.github.com>
2023-11-09 15:19:40 +0100

    added premake5

--- README.md
+++ README.md
@@ -3,7 +3,13 @@ MAI WS2023 EAD demo game engine
 
 ai23m013
 
----
-Uses spdlog: https://github.com/gabime/spdlog/
+## Building the project
+1. run `GenerateProjects.bat`
 
+---
+Uses **spdlog**: https://github.com/gabime/spdlog/
 Copyright (c) 2016 Gabi Melman.
+
+Uses **premake5**: https://github.com/premake/
+Copyright (c) 2003-2022 Jason Perkins and individual contributors.
+All rights reserved.
---
+++ premake5.lua
@@ -0,0 +1,96 @@
+workspace "DemoEngine"
+	architecture 'x64'
+
+	configurations {
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "DemoEngine"
+	location "DemoEngine"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files {
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs {
+		"%{prj.name}/thirdparty/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines {
+			"DEMOENGINE_PLATFORM_WINDOWS",
+			"DEMOENGINE_BUILD_DLL"
+		}
+
+		postbuildcommands {
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "DEMOENGINE_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "DEMOENGINE_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "DEMOENGINE_DIST"
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files {
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs {
+		"DemoEngine/thirdparty/spdlog/include",
+		"DemoEngine/src"
+	}
+
+	links {
+		"DemoEngine"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines {
+			"DEMOENGINE_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "DEMOENGINE_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "DEMOENGINE_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "DEMOENGINE_DIST"
+		optimize "On"
